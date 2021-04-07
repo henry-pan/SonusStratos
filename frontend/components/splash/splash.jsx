a50cb7ebcc3a76fba1d5aa0c67d345d05447b341
Henry <henry-pan@users.noreply.github.com>
2021-04-06 18:30:02 -0700

    Migrate static assets to asset pipeline

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -28,62 +28,62 @@ export default ({ currentUser, logout, openModal }) => {
         <h1>Hear what's trending for free in the SonusStratos community</h1>
         <div className="splash-trending-items">
           <div className="splash-track">
-            <img src="./assets/album/gop.png" />
+            <img src={window.gop} />
             <h2>Cradle of Knowledge</h2>
             <h3>Gate of Providence</h3>
           </div>
           <div className="splash-track">
-            <img src="./assets/album/fireemblem.png" />
+            <img src={window.fireemblem} />
             <h2>God Shattering Star</h2>
             <h3>Fire Emblem: Three Houses</h3>
           </div>
           <div className="splash-track">
-            <img src="./assets/album/nierautomata.jpg" />
+            <img src={window.nierAutomata} />
             <h2>Vague Hope (Cold Rain)</h2>
             <h3>NieR: Automata</h3>
           </div>
           <div className="splash-track">
-            <img src="./assets/album/ys.jpg" />
+            <img src={window.ys} />
             <h2>Genesis Beyond the Beginning</h2>
             <h3>Ys Origin</h3>
           </div>
           <div className="splash-track">
-            <img src="./assets/album/atelier1.jpg" />
+            <img src={window.atelier1} />
             <h2>Astarte</h2>
             <h3>Atelier Meruru</h3>
           </div>
           <div className="splash-track">
-            <img src="./assets/album/pokemon.png" />
+            <img src={window.pokemon} />
             <h2>Vast Poni Canyon</h2>
             <h3>Pokemon Sun and Moon</h3>
           </div>
           <div className="splash-track">
-            <img src="./assets/album/dreamshot.png" />
+            <img src={window.dreamshot} />
             <h2>Dreamshot Dissonance</h2>
             <h3>Dreamshot Dissonance</h3>
           </div>
           <div className="splash-track">
-            <img src="./assets/album/atelier2.jpg" />
+            <img src={window.atelier2} />
             <h2>Nefertiti (Ver. MMXI)</h2>
             <h3>Atelier Meruru</h3>
           </div>
           <div className="splash-track">
-            <img src="./assets/album/nier.png" />
+            <img src={window.nier} />
             <h2>Shadowlord</h2>
             <h3>NieR</h3>
           </div>
           <div className="splash-track">
-            <img src="./assets/album/smash.png" />
+            <img src={window.smash} />
             <h2>Stickerbrush Symphony</h2>
             <h3>Super Smash Bros. Ultimate</h3>
           </div>
           <div className="splash-track">
-            <img src="./assets/album/octopath.jpg" />
+            <img src={window.octopath} />
             <h2>They Who Govern Reason</h2>
             <h3>Octopath Traveller</h3>
           </div>
           <div className="splash-track">
-            <img src="./assets/album/srw.jpg" />
+            <img src={window.srw} />
             <h2>Ash to Ash</h2>
             <h3>Super Robot Wars OG</h3>
           </div>

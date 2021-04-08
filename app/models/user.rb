62939085db460e08ce79184976c3bdf2f8b21a13
Henry <henry-pan@users.noreply.github.com>
2021-04-07 17:51:07 -0700

    Add model validation for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   validates :username, :session_token, presence: true, uniqueness: true
   validates :password_digest, presence: true
   validates :password, length: { minimum: 6, allow_nil: true }
+  validates :email, length: { minimum: 1, allow_nil: true }, uniqueness: true
 
   attr_reader :password
   after_initialize :ensure_session_token

57f30e49c69f97b3ebd70efb46de42bf6789801b
Henry <henry-pan@users.noreply.github.com>
2021-04-12 03:50:27 -0700

    Complete most of upload page

--- frontend/components/upload/upload.jsx
+++ frontend/components/upload/upload.jsx
@@ -4,10 +4,14 @@ import NavBarContainer from "../navbar/navbar_container";
 class Upload extends React.Component {
   constructor(props) {
     super(props);
+
+    let uploaderId = undefined;
+    if (this.props.currentUser) uploaderId = this.props.currentUser.id;
+
     this.state = {
       title: "",
       description: "",
-      uploader_id: this.props.currentUser.id
+      uploader_id: uploaderId
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -25,7 +29,7 @@ class Upload extends React.Component {
 
   renderErrors(){
     return (
-      <ul>
+      <ul className="upload-form-errors">
         {this.props.errors.map((error, i) => (
           <li key={`error-${i}`}>{error}</li>
         ))}
@@ -34,19 +38,45 @@ class Upload extends React.Component {
   }
 
   render() {
+    const uploadForm = (
+      <div className="upload-form-container">
+        <nav className="upload-form-nav">
+          <span className="upload-form-nav-item">Basic info</span>
+        </nav>
+        <div className="upload-form-body">
+          <img className="upload-form-art" src={window.nierAutomata}/>
+          <form id="submit-upload-form" className="upload-form-form" onSubmit={this.handleSubmit}>
+            <span className="upload-form-label-req">Title</span>
+            <input className="upload-form-input" onChange={(e) => this.handleInput("title", e)} type="text" value={this.state.title} placeholder="Name your track"/>
+            <span>Description</span>
+            <textarea className="upload-form-input" onChange={(e) => this.handleInput("description", e)} value={this.state.description} placeholder="Describe your track"/>
+            {this.renderErrors()}
+          </form>
+        </div>
+        <div className="upload-form-submit">
+          <span className="upload-form-submit-req">Required fields</span>
+          <div className="upload-form-button-container">
+            <button form="submit-upload-form" type="submit">Save</button>
+          </div>
+        </div>
+      </div>
+    );
+
     return (
       <>
       <NavBarContainer />
-      <div className="content">
-        <div>
-          {this.props.currentUser ? <h1>Upload something, {this.props.currentUser.username}!</h1> : <h1>Log in to upload.</h1>}
-          <form onSubmit={this.handleSubmit}>
-            <input onChange={(e) => this.handleInput("title", e)} type="text" value={this.state.title} />
-            <textarea onChange={(e) => this.handleInput("description", e)} value={this.state.description} />
-            {this.renderErrors()}
-            <button>Upload</button>
-          </form>
+      <div className="upload-content">
+        <nav className="upload-nav">
+          <span className="upload-nav-item">Upload</span>
+        </nav>
+        <div className="upload-container">
+          {this.props.currentUser && uploadForm}
         </div>
+        <footer className="upload-footer">
+          <p>By uploading, you agree to not upload inappropriate tracks or copyrighted materials... or else.</p>
+          <span><a href="https://github.com/henry-pan">GitHub</a> - <a href="https://www.linkedin.com/in/henry-pan/">LinkedIn</a></span>
+          <p>Language: English (US)</p>
+        </footer>
       </div>
       </>
     )

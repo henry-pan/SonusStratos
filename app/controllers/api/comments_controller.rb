f6edebb34c85b2b19851da1660de867af4ca8231
Henry <henry-pan@users.noreply.github.com>
2021-05-23 22:30:56 -0700

    Add controller for comment

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,4 +1,23 @@
 class Api::CommentsController < ApplicationController
 
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      render :show
+    else
+      render json: @comment.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @comment = Comment.find_by(id: params[:id])
+    @comment.destroy
+    render json: {}
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:comment_body, :track_id)
+  end
 
 end

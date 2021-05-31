73f7521e457f074c63edbf06074933eef6e07975
Henry <henry-pan@users.noreply.github.com>
2021-05-31 15:39:35 -0700

    Add new seeds

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -31,62 +31,62 @@ export default ({ currentUser, logout, openModal }) => {
           <div className="splash-track">
             <img src={window.gop} />
             <h2>Cradle of Knowledge</h2>
-            <h3>Gate of Providence</h3>
+            <h3>StudioXehryn</h3>
           </div>
           <div className="splash-track">
-            <img src={window.meruru} />
-            <h2>Astarte</h2>
-            <h3>Atelier Meruru</h3>
-          </div>
-          <div className="splash-track">
-            <img src={window.fsn} />
-            <h2>New Dawn</h2>
-            <h3>Fate/stay night: Realta Nua</h3>
-          </div>
-          <div className="splash-track">
-            <img src={window.fates} />
-            <h2>Thorn in You (Roar)</h2>
-            <h3>Fire Emblem Fates</h3>
+            <img src={window.ds3} />
+            <h2>Vordt of the Boreal Valley</h2>
+            <h3>AshenOne</h3>
           </div>
           <div className="splash-track">
             <img src={window.aria} />
             <h2>Ruined Castle Corridor</h2>
-            <h3>Castlevania: Aria of Sorrow</h3>
+            <h3>UCSomaCruz</h3>
           </div>
           <div className="splash-track">
-            <img src={window.ryza} />
-            <h2>Grain Rain, Wheat Wind</h2>
-            <h3>Atelier Ryza</h3>
+            <img src={window.meruru} />
+            <h2>Astarte</h2>
+            <h3>Meruru</h3>
+          </div>
+          <div className="splash-track">
+            <img src={window.fates} />
+            <h2>Thorn in You (Roar)</h2>
+            <h3>SongstressAzura</h3>
+          </div>
+          <div className="splash-track">
+            <img src={window.kirby} />
+            <h2>Frozen Hillside</h2>
+            <h3>americankirby</h3>
           </div>
           <div className="splash-track">
             <img src={window.dreamshot} />
             <h2>Dreamshot Dissonance</h2>
-            <h3>Dreamshot Dissonance</h3>
+            <h3>StudioXehryn</h3>
           </div>
           <div className="splash-track">
             <img src={window.automata} />
             <h2>Vague Hope (Cold Rain)</h2>
-            <h3>NieR: Automata</h3>
+            <h3>devolapopola</h3>
           </div>
           <div className="splash-track">
-            <img src={window.ecclesia} />
-            <h2>An Empty Tome</h2>
-            <h3>Castlevania: Order of Ecclesia</h3>
-          </div>
-          <div className="splash-track">
-            <img src={window.kh3} />
-            <h2>Scala ad Caelum</h2>
-            <h3>Kingdom Hearts III</h3>
+            <img src={window.bw} />
+            <h2>Driftveil City</h2>
+            <h3>PeekAtYou</h3>
           </div>
           <div className="splash-track">
             <img src={window.ygo} />
             <h2>5D's Winning Theme</h2>
-            <h3>Yu-Gi-Oh! Duel Links</h3>
+            <h3>YuseiGo</h3>
           </div>
           <div className="splash-track">
-            <img src={window.threehouses} />
-            <h2>God Shattering Star</h2>
-            <h3>Fire Emblem: Three Houses</h3>
+            <img src={window.kh3} />
+            <h2>Vector to the Heavens -Xion-</h2>
+            <h3>Roxmas</h3>
+          </div>
+          <div className="splash-track">
+            <img src={window.ys7} />
+            <h2>Vacant Interference</h2>
+            <h3>AdolTheRed</h3>
           </div>
         </div>
       </section>

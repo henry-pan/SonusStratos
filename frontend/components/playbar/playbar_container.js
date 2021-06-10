fdf381cfe8f795afe3faa3ad5cba59a0895cfb3d
Henry <henry-pan@users.noreply.github.com>
2021-06-10 02:59:37 -0700

    Delete random console junk saved to repo

--- frontend/components/playbar/playbar_container.js
+++ frontend/components/playbar/playbar_container.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 import { receivePlayTrack, playTrack, pauseTrack } from '../../actions/play_actions';
 import PlayBar from './playbar';
 
-const mapStateToProps = (state, ownProps) => ({
+const mapStateToProps = state => ({
   currentUser: state.entities.users[state.session.id],
   currentTrack: state.ui.playbar.track,
   isPlaying: state.ui.playbar.isPlaying,
